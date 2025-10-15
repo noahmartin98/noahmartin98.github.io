9abd8a0ffb7a6df8021859534523a1dc2df3abff
noahmartin98 <noah.martin2@snhu.edu>
2025-10-15 07:55:13 -0400

    Updated

--- api/index.php
+++ api/index.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         'message' => 'Hello, World!'
     );
 
-    echo json_encode($response);
+    #echo json_encode($response);
 } else {
 
     http_response_code(405);
