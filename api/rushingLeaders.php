ca249ac02949b17127ead6bacb66251f8083531e
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-10-15 19:44:08 -0400

    Update rushingLeaders.php

--- api/rushingLeaders.php
+++ api/rushingLeaders.php
@@ -8,7 +8,7 @@
 <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
-<script src="myjavascript.js"></script>
+<script src="../myjavascript.js"></script>
 </head>
 <body>
 
