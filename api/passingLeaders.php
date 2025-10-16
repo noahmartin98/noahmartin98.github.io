4da3d21b74a6a388b6fb0c9b216de87b1749b4c5
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-10-15 21:04:23 -0400

    Update passingLeaders.php

--- api/passingLeaders.php
+++ api/passingLeaders.php
@@ -92,7 +92,7 @@ if ($result->num_rows > 0) {
         $playerid = $row["Player_ID"];
         echo "<tr>";
         echo "<td>".$cur_rank."</td>";
-        echo "<td class='link'><a class='leader' href='/playerPagePass.php?playerid=" . $playerid . "'>" . $row["Player_Name"] . "</a></td>";
+        echo "<td class='link'><a class='leader' href='/api/playerPagePass.php?playerid=" . $playerid . "'>" . $row["Player_Name"] . "</a></td>";
         echo "<td>". $row["Poss"]."</td>";
         echo "<td>". $row["Teams"]."</td>";
         echo "<td>". $row["Gms"]."</td>";
