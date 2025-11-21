925fe32b716a8a6d655beaab4d68d08bbb77a29f
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-11-21 11:11:20 -0500

    Update playerPageRec.php

--- api/playerPageRec.php
+++ api/playerPageRec.php
@@ -34,6 +34,7 @@ if ($result->num_rows > 0) {
         <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
         <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
         <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
+        <li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
         <li><a href="/api/bracket.php">Playoff Brackets</a></li>
     </ul>
 </nav>
