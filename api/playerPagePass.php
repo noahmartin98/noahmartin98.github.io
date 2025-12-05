8f3444bf7ad58cb31d0bbb26814ff8d35d0a3d08
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-05 14:43:46 -0500

    Update playerPagePass.php

--- api/playerPagePass.php
+++ api/playerPagePass.php
@@ -114,6 +114,8 @@ if ($result->num_rows > 0) {
             <th>Week</th>
             <th>Date</th>
             <th>User</th>
+			<th></th>
+			<th>Opp</th>
             <th>Comp</th>
             <th>Att</th>
             <th>Yds</th>
@@ -124,14 +126,26 @@ if ($result->num_rows > 0) {
 
 <?php
 
-$sql = "SELECT game.Season, team.Abbr, game.Week_Round, game.Game_Date, t1.team_user, Comp, Att, Yds, TD, INTR
+$sql = "SELECT game.Season, opp.Abbr, game.Week_Round, game.Game_Date, t1.team_user, 
+		CASE
+			WHEN (t1.home_away = 'Home')
+				THEN 'vs.'
+			WHEN (t1.home_away = 'Away')
+				THEN '@'
+			ELSE ''
+		END AS loc,
+    opp.Abbr, Comp, Att, Yds, TD, INTR
     FROM pass_statline
     INNER JOIN game ON pass_statline.Game_ID = game.Game_ID
     INNER JOIN team ON pass_statline.Team_ID = team.Team_ID
     JOIN team_statline t1
-	ON t1.game_id = pass_statline.game_id
-    AND t1.team_id = pass_statline.team_id
-    WHERE Player_ID = $playerid;";
+		ON t1.game_id = pass_statline.game_id
+		AND t1.team_id = pass_statline.team_id
+	JOIN team_statline t2
+		ON t2.game_id = pass_statline.game_id
+		AND t2.team_id <> pass_statline.team_id
+	JOIN team opp ON t2.team_id = opp.team_id
+    WHERE Player_ID = $playerid";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -144,6 +158,8 @@ if ($result->num_rows > 0) {
         echo "<td>". $row["Week_Round"]."</td>";
         echo "<td>". $row["Game_Date"]."</td>";
         echo "<td>". $row["team_user"]."</td>";
+		echo "<td>". $row["loc"]."</td>";
+		echo "<td>". $row["Abbr"]."</td>";
         echo "<td>". $row["Comp"]."</td>";
         echo "<td>". $row["Att"]."</td>";
         echo "<td>". $row["Yds"]."</td>";
