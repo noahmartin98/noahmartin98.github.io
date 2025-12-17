985a1f0546cc8eb8ceeb0897b47246410d8917ef
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:35:08 -0500

    Update playerPageRush.php

--- api/playerPageRush.php
+++ api/playerPageRush.php
@@ -26,18 +26,9 @@ if ($result->num_rows > 0) {
     echo "0 results";
 }
 
-?>
+require 'navbar.php';
 
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
-        <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
-        <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/bracket.php">Playoff Brackets</a></li>
-    </ul>
-</nav>
+?>
 
 <div class="header-container">
     <h1><?php echo $playername?></h1>
