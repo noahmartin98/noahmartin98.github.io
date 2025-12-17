7d7b0264169e0724726661b888b7161030b335bf
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:33:50 -0500

    Update gamePage.php

--- api/gamePage.php
+++ api/gamePage.php
@@ -11,18 +11,10 @@ require 'databaseConnect.php';
 if (isset($_GET['gameid'])) {
     $gameid = $_GET['gameid'];
 }
+
+require 'navbar.php';
 ?>
 
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
-        <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
-        <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/bracket.php">Playoff Brackets</a></li>
-    </ul>
-</nav>
 
 <?php
 
