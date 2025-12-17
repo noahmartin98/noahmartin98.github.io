53addbbfcb278637de7c69efd6a0370ca0a772fe
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:41:53 -0500

    Update navbar.php

--- api/navbar.php
+++ api/navbar.php
@@ -1,12 +1,16 @@
 
+<?php $season = $season ?? null; ?>
+
 <nav class="navbar">
     <ul class="nav-links">
         <li><a href="../home.html">Home</a></li>
         <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
         <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
         <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		    <li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/scoreboard.php">Scores</a></li>
+		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
+        <a href="/api/scoreboard.php<?= $season ? '?season=' . urlencode($season) : '' ?>">
+		    Scores
+		</a>
         <li><a href="/api/bracket.php">Playoff Brackets</a></li>
     </ul>
 </nav>
