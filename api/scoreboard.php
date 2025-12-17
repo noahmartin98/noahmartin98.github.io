37cdeb67b1294d5f70c613d9955bcca046ab4e10
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:35:42 -0500

    Update scoreboard.php

--- api/scoreboard.php
+++ api/scoreboard.php
@@ -9,18 +9,9 @@
 require 'databaseConnect.php';
 
 $season = $_GET['season'] ?? '2024';
-?>
 
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
-        <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
-        <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/bracket.php">Playoff Brackets</a></li>
-    </ul>
-</nav>
+require 'navbar.php';
+?>
 
 <div class="header-container">
 
