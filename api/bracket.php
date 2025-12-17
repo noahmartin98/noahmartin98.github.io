6115c55f459949c6b1f6b630febd3be37618b473
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:32:49 -0500

    Update bracket.php

--- api/bracket.php
+++ api/bracket.php
@@ -9,20 +9,11 @@
 </head>
 <body>
 
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
-        <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
-        <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/bracket.php">Playoff Brackets</a></li>
-    </ul>
-</nav>
-
 <?php
 
 require 'databaseConnect.php';
+
+require 'navbar.php';
 
 
 $season = $_GET['season'] ?? '2015';
