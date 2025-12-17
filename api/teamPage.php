e1dcc5db3e3ad30792f583d41fb77c8a56e8b680
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2025-12-17 09:23:28 -0500

    Update teamPage.php

--- api/teamPage.php
+++ api/teamPage.php
@@ -28,18 +28,10 @@ if ($result->num_rows > 0) {
 
 $season = $_GET['season'] ?? '2024';
 
-?>
 
-<nav class="navbar">
-    <ul class="nav-links">
-        <li><a href="../home.html">Home</a></li>
-        <li><a href="/api/passingLeaders.php">Passing Leaders</a></li>
-        <li><a href="/api/rushingLeaders.php">Rushing Leaders</a></li>
-        <li><a href="/api/receivingLeaders.php">Receiving Leaders</a></li>
-		<li><a href="/api/defLeaders.php">Defensive Leaders</a></li>
-        <li><a href="/api/bracket.php">Playoff Brackets</a></li>
-    </ul>
-</nav>
+require 'navbar.php';
+
+?>
 
 <!--Header with team name and season select-->
 <div class="header-container">
