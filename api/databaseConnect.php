4ccf44301d79c50ec959d0e6d60bc4cb80f527c5
noahmartin98 <154024612+noahmartin98@users.noreply.github.com>
2026-09-25 18:29:03 -0400

    Update databaseConnect.php

--- api/databaseConnect.php
+++ api/databaseConnect.php
@@ -1,8 +1,8 @@
 
 <?php
-$servername = "football-db.cnsw24uw47hg.us-east-2.rds.amazonaws.com";
-$username = "admin";
-$password = "Almostdone2025";
+$servername = "mysql-c4b6f13-sportssim98-2816.g.aivencloud.com";
+$username = "avnadmin";
+$password = "AVNS_s9w_D_bs4m3e3bGzvGe";
 $dbname = "football_db";
 
 // Create connection
